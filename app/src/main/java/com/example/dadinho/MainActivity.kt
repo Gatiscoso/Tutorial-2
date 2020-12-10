5357899ff97f657fd5a9dca3871ed5afa191a6f4
Saiki Pacheco <spf.eai19@uea.edu.br>
2020-12-09 21:03:37 -0400

    Tutorial 3

--- app/src/main/java/com/example/dadinho/MainActivity.kt
+++ app/src/main/java/com/example/dadinho/MainActivity.kt
@@ -2,20 +2,22 @@ package com.example.dadinho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val num = dadinho()
+        val num = dado()
 
-        texto.text = "toque para jogar seu dado de 6 lados"
+        textin.text = "toque para jogar seu dado de 6 lados"
 
         button.setOnClickListener()
         {
 
-            texto.text = num.numdado
+            textin.text = num.numdado.toString()
 
         }
     }
---
+++ app/src/main/java/com/example/dadinho/dado.kt
@@ -0,0 +1,10 @@
+package com.example.dadinho
+
+class dado {
+
+    var numdado = roll()
+}
+
+fun roll(): Int {
+    return (1..6).shuffled().first()
+}
